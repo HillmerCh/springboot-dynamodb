2665e4cf061f2a51245d7a109078c0aa144f3a11
HillmerCh <hilac@msn.com>
2020-08-31 12:36:52 -0500

    Updating readme, code cleanup

--- src/main/java/org/hillmerch/library/data/config/DynamoDBConfig.java
+++ src/main/java/org/hillmerch/library/data/config/DynamoDBConfig.java
@@ -1,23 +1,15 @@
 package org.hillmerch.library.data.config;
 
-import com.amazonaws.auth.AWSCredentialsProvider;
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.client.builder.AwsClientBuilder;
-import com.amazonaws.regions.Regions;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import org.socialsignin.spring.data.dynamodb.repository.config.EnableDynamoDBRepositories;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.util.StringUtils;
-import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
-import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.BasicAWSCredentials;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClient;
 
 @Configuration
 @EnableDynamoDBRepositories(basePackages = "org.hillmerch.library.data.repositories")
@@ -36,12 +28,8 @@ public class DynamoDBConfig {
 	@Value("${amazon.aws.region}")
 	private String amazonAWSRegion;
 
-	@Autowired
-	private ApplicationContext context;
-
 	@Bean
 	public AmazonDynamoDB amazonDynamoDB() {
-
 		AmazonDynamoDB amazonDynamoDB
 				= AmazonDynamoDBClientBuilder.standard()
 				.withEndpointConfiguration(new AwsClientBuilder.EndpointConfiguration (amazonDynamoDBEndpoint, amazonAWSRegion) )
@@ -50,14 +38,4 @@ public class DynamoDBConfig {
 		return amazonDynamoDB;
 	}
 
-
-	@Bean
-	public AWSCredentials amazonAWSCredentials() {
-		return new BasicAWSCredentials(amazonAWSAccessKey, amazonAWSSecretKey);
-	}
-
-	@Bean(name = "mvcHandlerMappingIntrospectorCustom")
-	public HandlerMappingIntrospector mvcHandlerMappingIntrospectorCustom() {
-		return new HandlerMappingIntrospector(context);
-	}
 }
